88bbf6fcf06b8a049f86f6f2b5a6e88e9ce9ec13
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-12-28 16:11:06 +0500

    shsf

--- Tyuiu.MacuacuaRa.Sprint5.Task0.V5.Lib/DataService.cs
+++ Tyuiu.MacuacuaRa.Sprint5.Task0.V5.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MacuacuaRa.Sprint5.Task0.V5.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double result = Math.Round(Math.Log((x+1)/(x+2)), 3);
+            double result = Math.Round(Math.Log((x+1.0)/(x+2.0)), 3);
 
             return result.ToString();
         }
