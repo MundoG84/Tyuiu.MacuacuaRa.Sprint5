70c55e537d24e10e4f0f6322a03766822d19a264
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-12-28 16:14:44 +0500

    cksdam

--- Tyuiu.MacuacuaRa.Sprint5.Task2.V12.Lib/DataService.cs
+++ Tyuiu.MacuacuaRa.Sprint5.Task2.V12.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MacuacuaRa.Sprint5.Task2.V12.Lib
         {
             string output = "";
 
-            // Процесс обработки массива
+            // Процесс обработки мас
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
